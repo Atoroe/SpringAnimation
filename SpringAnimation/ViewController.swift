c9a0e47b890e0da1ddaccd239abd160f21fa7622
Artiom P <apoluyanovich@gmail.com>
2021-04-27 15:59:16 +0300

    Initial Commit

---
+++ SpringAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimation
+//
+//  Created by Artiom Poluyanovich on 27.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
